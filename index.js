7f2e7a896ac0b72bf49d5a93ba902f299ef59339
shiba2k <70843061+shiba2k@users.noreply.github.com>
2021-12-23 21:42:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
-const express = require('express');
-const app = express();
-const SocketIO = require("socket.io");
-const ejs = require("ejs");
+const mineflayer = require("mineflayer");
 
-var mc = require('minecraft-protocol');
-var client = mc.createClient({
-  host: "Suhibar.aternos.me",
+const bot = mineflayer.createBot({
+	host: "Suhibar.aternos.me",
 	port: 51608,
 	username: "Subnc",
 });
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-	res.render('index');
-});
-	
-server = app.listen();
-SocketIO(server);
+function lookAtNearestPlayer() {
+	const playerFilter = (entity) => entity.type === "player";
+	const playerEntity = bot.nearestEntity(playerFilter);
 
-client.on('chat', function(packet) {
-  	var jsonMsg = JSON.parse(packet.message);
-	console.log(jsonMsg);
-	setTimeout(io.sockets.emit,1000,'a',jsonMsg);
-  	
-	/*if(!jsonMsg.extra) return;
-	if(!jsonMsg.extra[0].text.startsWith('<')) return;
-	var [original, username, msg] = jsonMsg.extra[0].text.toString().match(/(?:<(.+)> (.+))/);
-	if(username === client.username) return;*/
-});
+	if (!playerEntity) return;
+
+	const pos = playerEntity.position.offset(0, playerEntity.height, 0);
+	bot.lookAt(pos);
+}
+
+bot.on("physicTick", lookAtNearestPlayer);
